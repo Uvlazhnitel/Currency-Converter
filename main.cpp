9354aa936ef572e0781bc3bd7ba43db9616e3124
Ilya <ilya.yunkins@gmail.com>
2024-10-29 13:15:54 +0200

    table below input

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <wx/grid.h> // Подключаем wxGrid
 #include <wx/wx.h>
+#include <wx/mstream.h>
+#include <wx/bitmap.h>
+#include <wx/image.h>
 
 class MyFrame : public wxFrame
 {
@@ -13,7 +16,9 @@ private:
     wxComboBox* comboBox;
     wxStaticText* staticTextCtrl;
     wxStaticText* staticTextCtrl1;
-    wxGrid* grid;
+    wxGridSizer* gridSizer; // Изменено на wxGridSizer
+    wxBoxSizer* boxSizer;
+    wxImage image;
 
     double intValue;
 
@@ -41,7 +46,7 @@ bool MyApp::OnInit()
 MyFrame::MyFrame(const wxString& title)
     : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(600, 300))
 {
-    SetBackgroundColour(wxColour(81, 81, 195));
+    SetBackgroundColour(wxColour(48, 164, 193));
     wxPanel* panel = new wxPanel(this, wxID_ANY);
 
     textCtrl = new wxTextCtrl(panel, wxID_ANY, "", wxPoint(10, 10), wxSize(150, 30));
@@ -60,55 +65,61 @@ MyFrame::MyFrame(const wxString& title)
     comboBoxItems.Add("EUR-BTC");
 
     comboBox = new wxComboBox(panel, wxID_ANY, "Select Item", wxPoint(50, 50), wxSize(150, 30), comboBoxItems, wxCB_READONLY);
+
     comboBox->SetSelection(0);
-
-    wxBoxSizer* boxSizer = new wxBoxSizer(wxHORIZONTAL);
-
     comboBox->Bind(wxEVT_COMBOBOX, &MyFrame::OnComboBoxSelection, this);
 
+    boxSizer = new wxBoxSizer(wxHORIZONTAL);
     boxSizer->Add(textCtrl, 0, wxTOP | wxLEFT, 40);
     boxSizer->Add(comboBox, 0, wxALL, 30);
     boxSizer->Add(textCtrl1, 0, wxTOP , 40);
 
-    panel->SetSizer(boxSizer);
+    gridSizer = new wxGridSizer(3, 3, 2, 2); 
+    for (int i = 1; i <= 9; ++i) {
+        wxStaticText* label = new wxStaticText(panel, wxID_ANY, wxString::Format("Text %d", i)); 
+        gridSizer->Add(label, 0, wxALIGN_CENTER);
+    }
 
+    wxBoxSizer* mainSizer = new wxBoxSizer(wxVERTICAL);
+    mainSizer->Add(boxSizer, 0, wxALL | wxEXPAND, 10);
+    mainSizer->Add(gridSizer, 0, wxALL | wxEXPAND, 10);
+    
+    panel->SetSizer(mainSizer);
+
+
+    
     Refresh();
-
 }
 
-
-void MyFrame::UpdateConversion(wxCommandEvent& event){
+void MyFrame::UpdateConversion(wxCommandEvent& event) {
     int selection = comboBox->GetSelection();
 
-    if(selection == 1){
+    if (selection == 1) {
         staticTextCtrl1->SetLabel("USD:");
-        textCtrl1->SetValue(wxString::Format("%.2f", intValue*1.08));
-    } else if(selection == 2){
+        textCtrl1->SetValue(wxString::Format("%.2f", intValue * 1.08));
+    } else if (selection == 2) {
         staticTextCtrl1->SetLabel("RUB:");
-        textCtrl1->SetValue(wxString::Format("%.2f", intValue*104.55));
-    } else if (selection == 3){
+        textCtrl1->SetValue(wxString::Format("%.2f", intValue * 104.55));
+    } else if (selection == 3) {
         staticTextCtrl1->SetLabel("BTC:");
-        textCtrl1->SetValue(wxString::Format("%.6f", intValue/61,956));
-    }else {
+        textCtrl1->SetValue(wxString::Format("%.6f", intValue / 61956));
+    } else {
         textCtrl1->Clear();
         staticTextCtrl1->SetLabel("");
     }
 }
+
 void MyFrame::OnComboBoxSelection(wxCommandEvent& event) {
     UpdateConversion(event);
 }
 
-
-
-void MyFrame::OnTextChanged(wxCommandEvent& event){
+void MyFrame::OnTextChanged(wxCommandEvent& event) {
     wxString text = textCtrl->GetValue();
 
-    if(text.ToDouble(&intValue)){
+    if (text.ToDouble(&intValue)) {
         textCtrl1->SetValue(wxString::Format("%.2f", intValue));
         UpdateConversion(event);
-    }else if(text.IsEmpty()){
+    } else if (text.IsEmpty()) {
         textCtrl1->Clear();
     }
 }
-
-
